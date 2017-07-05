056ab04be55363871f1937bb9063ba6f19e37ec5
attallah sara <attallah.sara@gmail.com>
2017-07-05 15:49:51 +0200

    contact mobile start

--- src/view/contact/Contact.vue
+++ src/view/contact/Contact.vue
@@ -1,6 +1,27 @@
 <template lang="html">
-  <div class="contact">
-    <h1>Contact</h1>
+  <div class="container">
+    <div class="contact">
+      <div class="row">
+        <div class="col-xs-12 col-md-3">
+          <h1>Contact</h1>
+            <div class="contact-block">
+              <h4>Adresse</h4>
+              <p>Hotel Dieu building A1 - 6th floor </p>
+              <p>1 Parvis Notre-Dame - Pl. Jean-Paul II</p>
+              <p>75004 Paris, France</p>
+            </div>
+
+          <div class="contact-block">
+            <h4>Mail</h4>
+            <p>contact@echopen.org</p>
+          </div>
+          <div class="contact-block">
+            <h4>Téléphone</h4>
+            <p class="tel">01.64.28.92.67</p>
+          </div>
+        </div>
+        </div>
+      </div>
   </div>
 </template>
 
