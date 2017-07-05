2e0d228a5ba75990be72c7ac78677ee63bb720bc
nicolaszamarreno <zamarreno.nicolas@gmail.com>
2017-07-05 14:18:17 +0200

    Begin Home

--- src/components/header/header_controller.js
+++ src/components/header/header_controller.js
@@ -1,11 +1,9 @@
 import Navigation from '@/components/navigation/Navigation';
-import Logo from '@/assets/logo2x.png';
 
 export default{
   name: 'header',
   data() {
     return {
-      assetsPath: Logo,
     };
   },
   components: {
--- src/view/home/Home.vue
+++ src/view/home/Home.vue
@@ -1,20 +1,38 @@
 <template lang="html">
-  <div class="hero">
-    <div class="container">
-        <div class="row align-items-center">
-        <div class="col-md-8 hero__baseline">
-            <h1>
-                La <span>communauté Échopen</span> s’engage à sauver des vies!
-            </h1>
-            <p>
-                Iam in altera philosophiae parte. quae est quaerendi ac disserendi, quae logikh dicitur, iste vester plane, ut mihi quidem videtur, inermis ac nudus est. tollit definitiones, nihil de dividendo ac partiendo docet, non quo modo efficiatur concludaturque ratio tradit, non qua via captiosa solvantur ambigua distinguantur ostendit; iudicia rerum in sensibus ponit, quibus si semel aliquid falsi pro vero probatum sit, sublatum esse omne iudicium veri et falsi putat.
-            </p>
-            <a href="" class="hero__button">Découvrir</a>
+<main class="main">
+    <section class="hero">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-8 hero__baseline">
+                    <h1>
+                        La <span>communauté Échopen</span> s’engage à sauver des vies!
+                    </h1>
+                    <p>
+                        Iam in altera philosophiae parte. quae est quaerendi ac disserendi, quae logikh dicitur, iste vester plane, ut mihi quidem videtur, inermis ac nudus est. tollit definitiones, nihil de dividendo ac partiendo docet, non quo modo efficiatur concludaturque ratio tradit, non qua via captiosa solvantur ambigua distinguantur ostendit; iudicia rerum in sensibus ponit, quibus si semel aliquid falsi pro vero probatum sit, sublatum esse omne iudicium veri et falsi putat.
+                    </p>
+                    <a href="" class="hero__button">Découvrir</a>
+                </div>
+            </div>
         </div>
+    </section>
+    <section class="revolution section">
+        <div class="container">
+            <h2 class="text-center">Un produit <span>révolutionnaire</span></h2>
+            <h4 class="text-center">Le futur est à notre portée</h4>
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="a" alt="a">
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <h4 class="card__title"></h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
 
-        </div>
-    </div>
-  </div>
 </template>
 
 <script src="./home_controller.js">
