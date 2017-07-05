9b1b0d11e6c18b15c4e0e01e722bd927c4de7c97
Leinad93160 <daniel.dupont@hetic.net>
2017-07-05 15:34:32 +0200

    [feature:footer] add the footer styles

--- src/components/footer/footer_controller.js
+++ src/components/footer/footer_controller.js
@@ -1,10 +1,13 @@
+import Logo from '@/assets/logo2x.png';
 import Navigation from '@/components/navigation/Navigation';
 import Newsletter from './newsletter/Newsletter';
 
 export default {
   name: 'footer',
   data() {
-    return {};
+    return {
+      img: Logo,
+    };
   },
   components: {
     navigation: Navigation,
