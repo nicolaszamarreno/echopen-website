899366bd6b2ed684a6e010ac7d576f32b6e69417
nicolaszamarreno <zamarreno.nicolas@gmail.com>
2017-07-05 22:29:48 +0200

    Add section and begin responsive

--- src/view/home/Home.vue
+++ src/view/home/Home.vue
@@ -20,12 +20,12 @@
             <h2 class="title__medium text-center">Un produit <strong>révolutionnaire</strong></h2>
             <h4 class="title__small text-center">Le futur est à notre portée</h4>
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-lg-4">
                     <div class="revolution__probe text-center">
                         <img class="revolution__probe" src="../../../src/assets/home_probe.svg" alt="Probe">
                     </div>
                 </div>
-                <div class="col-md-4 d-flex align-items-center">
+                <div class="col-lg-4 col-md-6 d-flex align-items-center">
                     <div class="card">
                         <h4 class="card__title">Portatif</h4>
                         <p class="text__regular card__text">
@@ -33,7 +33,7 @@
                         </p>
                     </div>
                 </div>
-                <div class="col-md-4 d-flex align-items-center">
+                <div class="col-lg-4 col-md-6 d-flex align-items-center">
                     <div class="card">
                         <h4 class="card__title">Portatif</h4>
                         <p class="text__regular card__text">
@@ -42,26 +42,26 @@
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-4 d-flex align-items-center">
-                    <div class="card">
-                        <h4 class="card__title">Portatif</h4>
-                        <p class="text__regular card__text">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisinuli.
-                        </p>
-                    </div>
-                </div>
-                <div class="col-md-4 d-flex align-items-center">
-                    <div class="card">
-                        <h4 class="card__title">Portatif</h4>
-                        <p class="text__regular card__text">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisinuli.
-                        </p>
-                    </div>
-                </div>
-                <div class="col-md-4 revolution__connect">
+            <div class="row flex-row-reverse">
+                <div class="col-lg-4 revolution__connect">
                     <div class=" text-center">
                         <img class="revolution__connect" src="../../../src/assets/home_connect.svg" alt="Probe">
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 d-flex align-items-center">
+                    <div class="card">
+                        <h4 class="card__title">Portatif</h4>
+                        <p class="text__regular card__text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisinuli.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 d-flex align-items-center">
+                    <div class="card">
+                        <h4 class="card__title">Portatif</h4>
+                        <p class="text__regular card__text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisinuli.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -69,13 +69,17 @@
     </section>
     <section class="home-save">
         <div class="container">
-            <h1 class="title__big title__big--white">
-                <strong>Des milliards de personne a sauver</strong>
-            </h1>
-            <p class="text__regular text__regular--white">
-                Iam in altera philosophiae parte. quae est quaerendi ac disserendi, quae logikh dicitur, iste vester plane, ut mihi quidem videtur, inermis ac nudus est. tollit definitiones, nihil de dividendo ac partiendo docet, non quo modo efficiatur concludaturque ratio tradit, non qua via captiosa solvantur ambigua distinguantur ostendit; iudicia rerum in sensibus ponit, quibus si semel aliquid falsi pro vero probatum sit, sublatum esse omne iudicium veri et falsi putat.
-            </p>
-            <a href="" class="button__regular">Découvrir</a>
+            <div class="row align-items-center">
+                <div class="col-md-12">
+                    <h1 class="title__big title__big--white">
+                    <strong>Des milliards de personne a sauver</strong>
+                    </h1>
+                    <p class="text__regular text__regular--white">
+                        Iam in altera philosophiae parte. quae est quaerendi ac disserendi, quae logikh dicitur, iste vester plane, ut mihi quidem videtur, inermis ac nudus est. tollit definitiones, nihil de dividendo ac partiendo docet, non quo modo efficiatur concludaturque ratio tradit, non qua via captiosa solvantur ambigua distinguantur ostendit; iudicia rerum in sensibus ponit, quibus si semel aliquid falsi pro vero probatum sit, sublatum esse omne iudicium veri et falsi putat.
+                    </p>
+                    <a href="" class="button__regular">Découvrir</a>
+                </div>
+            </div>
         </div>
     </section>
         <section class="home-pin">
@@ -127,6 +131,9 @@
         </div>
     </section>
     <section class="home-conference">
+        <svg class="home-conference__patternTop" data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1280 63.34">
+          <path d="M0,0H1280V63.34Q344.52-26.88,0,35.11Z" transform="translate(0 0)"/>
+        </svg>
         <div class="row no-gutters">
             <div class="col-md-6">
 
@@ -139,6 +146,21 @@
                     Iam in altera philosophiae parte. quae est quaerendi ac disserendi, quae logikh dicitur, iste vester plane, ut mihi quidem videtur, inermis ac nudus est. tollit definitiones, nihil de dividendo ac partiendo docet, non quo modo efficiatur concludaturque ratio tradit, non qua via captiosa solvantur ambigua distinguantur ostendit; iudicia rerum in sensibus ponit, quibus si semel aliquid falsi pro vero probatum sit, sublatum esse omne iudicium veri et falsi putat.
                 </p>
                 <a href="" class="button__regular">Découvrir</a>
+            </div>
+        </div>
+        <svg class="home-conference__patternBottom" data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1280 77.1">
+            <path d="M0,13.2Q480.75,114.75,1280,0V77.1H0Z" transform="translate(0 0)"/>
+        </svg>
+    </section>
+     <section class="home-newsletter">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-9 home-newsletter__baseline">
+                    <strong>Plus d’actus ?</strong> Rejoingnez la communauté et recevez nos actualités avec notre newsletter
+                </div>
+                <div class="col-lg-3 text-center">
+                    <a href="" class="button__color">A découvrir</a>
+                </div>
             </div>
         </div>
     </section>
